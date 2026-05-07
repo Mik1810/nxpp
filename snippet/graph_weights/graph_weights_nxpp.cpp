18f11038eda820431b66d3661f636789bbdbd58a
Michael Piccirilli <michaelpiccirilli3@gmail.com>
2026-05-07 22:56:00 +0200

    Modified graph weight snippet

--- snippet/graph_weights/graph_weights_nxpp.cpp
+++ snippet/graph_weights/graph_weights_nxpp.cpp
@@ -8,8 +8,8 @@ int main() {
     G.add_nodes_from({0, 1, 2, 3, 4});
     G.add_edge(0, 1, 5);
 
-    for (const auto& [u, v, w] : G.edges()) {
-        std::cout << "Edge from " << u << " to " << v << " with weight " << static_cast<int>(w) << "\n";
+    for (const auto& [u, v, w] : G.weighted_edges()) {
+        std::cout << "Edge from " << u << " to " << v << " with weight " << w << "\n";
     }
 
     return EXIT_SUCCESS;
